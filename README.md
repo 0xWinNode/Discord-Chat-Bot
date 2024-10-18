6a0538b76ab94c91e8921a8b02f382665b4b2aa1
Nofan Rambe <nrr15112002@gmail.com>
2024-10-18 20:39:02 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# DISCORD-CHAT-BOT
-Versatile Discord bot with quoting, random quotes, and reposting functionalities. Easy to set up and customize for your server.
+# Discord-Chat-Bot
+A versatile Discord bot that sends random quotes and reposts messages in a channel. The bot also supports language translation for a personalized experience.
+
+## Screenshot
+![Proof](https://i.ibb.co/3YFDYVx/Screenshot-at-Jan-11-00-08-44.png)
+
+## Features
+- Sends random quotes in the specified channel based on the chosen mode.
+- Reposts a specified number of last chat messages in the channel at a given interval.
+- Supports language translation for a dynamic and personalized touch.
+- Configurable settings for bot token, channel ID, mode, delay, and more.
+- Backup and history functionalities for ease of use.
+
+## Prerequisites
+Make sure you have [Node.JS](https://nodejs.org/) installed on your machine.
+
+## Installation
+1. Clone the repository.
+2. Install dependencies using `npm install`.
+3. Run the bot using `node index.js`.
+4. If an error occurs, you can delete the `.env` file and attempt the process again.
+
+## Usage
+To obtain your Discord token, follow these steps:
+1. Open Discord Web.
+2. Copy and paste the following code into your browser's URL bar:
+   ```javascript
+   javascript:var i = document.createElement('iframe');i.onload = function(){var localStorage = i.contentWindow.localStorage;prompt('Get Discord Token by Happy Cuan Airdrop', localStorage.getItem('token').replace(/["]+/g, ''));};document.body.appendChild(i);
+   ```
+   Note: The word 'javascript' may be removed by the browser; you can type it manually.
+   Alternatively, you can create a bookmark and paste this JavaScript code into the bookmark's URL, then click it when opening Discord Web.
+3. Run the script again using `node index.js` or paste your Discord token into `.env`, just like this:
+```env
+BOT_TOKEN= // Your Discord token (example: xxx.xxx.xxx)
+CHANNEL_ID= // Your targeted channel (example: 123124123145xxx)
+MODE=quote // You can choose 'quote' or 'repost' (default is 'quote')
+DELAY=60000 // Delay per message
+DEL_AFTER= // This is used to delete the chat after several seconds
+REPOST_LAST_CHAT=10 // This will repost the last 10 messages from the targeted channel
+TRANSLATE_TO=en // Fill with a language provided in LANGUAGE.md
+```
+
+## Configuration
+The bot can be configured via the `.env` file or by providing environment variables.
+
+### Supported Languages
+Check the [LANGUAGE.md](LANGUAGE.md) file for a list of supported languages.
+
+## Dependencies
+- [discord.js](https://discord.js.org/) - Discord API library.
+- [colors](https://www.npmjs.com/package/colors) - Adds colors to console.log.
+- [readline-sync](https://www.npmjs.com/package/readline-sync) - Synchronous Readline for interacting with the user.
+- [translate-google](https://www.npmjs.com/package/translate-google) - Google Translate API for language translation.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ index.js
@@ -0,0 +1,205 @@
+require('dotenv').config();
+const Discord = require('discord-simple-api');
+const colors = require('colors');
+const fs = require('fs');
+const readlineSync = require('readline-sync');
+const translate = require('translate-google');
+
+function shouldUsePreviousSettings() {
+  const envFileExists = fs.existsSync('.env');
+
+  if (envFileExists) {
+    const usePrevious = readlineSync.keyInYNStrict(
+      'Do you want to use the previous settings?'
+    );
+
+    if (usePrevious) {
+      try {
+        const envContent = fs.readFileSync('.env', 'utf8');
+        const envLines = envContent.split('\n');
+        envLines.forEach((line) => {
+          const [key, value] = line.split('=').map((entry) => entry.trim());
+          if (key && value) {
+            process.env[key] = value;
+          }
+        });
+        return true;
+      } catch (error) {
+        console.error('Error reading .env file:', error.message);
+      }
+    }
+  }
+
+  return false;
+}
+
+const usePreviousSettings = shouldUsePreviousSettings();
+
+let botToken = process.env.BOT_TOKEN;
+let channelId = process.env.CHANNEL_ID;
+let mode = process.env.MODE || 'quote';
+let delay = process.env.DELAY || 60000;
+let delAfter = process.env.DEL_AFTER || '';
+let repostLastChat = process.env.REPOST_LAST_CHAT || 10;
+let translateTo = process.env.TRANSLATE_TO || 'en';
+
+const quoteEN = require('./quotes-en.json');
+
+if (!usePreviousSettings) {
+  botToken = readlineSync.question('Enter your Discord bot token: ', {
+    defaultInput: botToken,
+  });
+  channelId = readlineSync.question('Enter the channel ID: ', {
+    defaultInput: channelId,
+  });
+  mode = readlineSync.question('Enter the mode (quote, repost): ', {
+    defaultInput: mode,
+  });
+  delay = readlineSync.questionInt(
+    'Enter the delay in milliseconds (1000ms = 1s): ',
+    { defaultInput: delay }
+  );
+  delAfter = readlineSync.question(
+    'Enter the delete delay in milliseconds (leave blank if not needed): ',
+    { defaultInput: delAfter }
+  );
+
+  while (!['quote', 'repost'].includes(mode.toLowerCase())) {
+    console.log(colors.red('Invalid mode! Please enter a valid mode.'));
+    mode = readlineSync.question('Enter the mode (quote, repost): ', {
+      defaultInput: mode,
+    });
+  }
+
+  while (isNaN(delay) || delay <= 0) {
+    console.log(colors.red('Invalid delay! Please enter a positive number.'));
+    delay = readlineSync.questionInt(
+      'Enter the delay in milliseconds (1000ms = 1s): ',
+      { defaultInput: delay }
+    );
+  }
+
+  while (delAfter !== '' && (isNaN(delAfter) || delAfter < 0)) {
+    console.log(
+      colors.red(
+        'Invalid delete delay! Please enter a non-negative number or leave it blank.'
+      )
+    );
+    delAfter = readlineSync.question(
+      'Enter the delete delay in milliseconds (leave blank if not needed): ',
+      { defaultInput: delAfter }
+    );
+  }
+
+  if (mode.toLowerCase() === 'repost') {
+    while (isNaN(repostLastChat) || repostLastChat <= 0) {
+      console.log(
+        colors.red(
+          'Invalid number of last chat messages for repost! Please enter a positive number.'
+        )
+      );
+      repostLastChat = readlineSync.questionInt(
+        'Enter the number of last chat messages for repost: ',
+        { defaultInput: repostLastChat }
+      );
+    }
+  }
+
+  translateTo = readlineSync.question(
+    'Enter the translation language code (check LANGUAGE.md) or leave blank for English (en): ',
+    {
+      defaultInput: translateTo,
+    }
+  );
+
+  const envData = `BOT_TOKEN=${botToken}\nCHANNEL_ID=${channelId}\nMODE=${mode}\nDELAY=${delay}\nDEL_AFTER=${delAfter}\nREPOST_LAST_CHAT=${repostLastChat}\nTRANSLATE_TO=${translateTo}`;
+  fs.writeFileSync('.env', envData);
+}
+
+let bot;
+
+try {
+  bot = new Discord(botToken);
+} catch (error) {
+  console.error(colors.red('Error initializing Discord bot:'), error.message);
+  process.exit(1);
+}
+
+async function getRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quoteEN.length);
+  const textToTranslate = quoteEN[randomIndex].text;
+
+  let translatedText = textToTranslate;
+
+  if (translateTo && translateTo.toLowerCase() !== 'en') {
+    try {
+      translatedText = await translate(textToTranslate, { to: translateTo });
+    } catch (error) {
+      console.error(colors.red('Translation error:'), error.message);
+    }
+  }
+
+  return translatedText;
+}
+
+bot
+  .getUserInformation()
+  .then((userInfo) => {
+    const me = userInfo.username + '#' + userInfo.discriminator;
+    console.log(colors.green('Logged in as %s'), me);
+  })
+  .catch((error) => {
+    console.error(colors.red('Error getting user information:'), error.message);
+  });
+console.log(colors.yellow('MODE: %s'), mode);
+
+function processMessage(_, contentCallback) {
+  bot.getMessagesInChannel(channelId, 1).then((messageData) => {
+    const hasMessages = messageData && messageData.length > 0;
+
+    if (!hasMessages) {
+      console.warn(
+        colors.yellow('No messages in the channel. Sending a message anyway.')
+      );
+    }
+
+    contentCallback(hasMessages ? messageData.reverse()[0].content : '').then(
+      (response) => {
+        bot.sendMessageToChannel(channelId, response).then((sentMessage) => {
+          const sentMessageContent = sentMessage.content;
+          console.log(
+            colors.green('[SEND][%s] %s'),
+            sentMessage.id,
+            sentMessageContent
+          );
+
+          if (delAfter) {
+            setTimeout(() => {
+              bot
+                .deleteMessageInChannel(channelId, sentMessage.id)
+                .then((deletedMessage) => {
+                  if (deletedMessage) {
+                    console.log(
+                      colors.red('[DELETE][%s] %s'),
+                      deletedMessage.id,
+                      sentMessageContent
+                    );
+                  } else {
+                    console.log(
+                      colors.red('[DELETE][%s] Deleted successfully'),
+                      sentMessage.id
+                    );
+                  }
+                })
+                .catch((error) => {
+                  console.error(colors.red('[DELETE] Error:'), error.message);
+                });
+            }, delAfter);
+          }
+        });
+      }
+    );
+  });
+}
+
+setInterval(() => processMessage(null, getRandomQuote), delay);
